54c8c1e544789e8d130157e9f8db2f6b27a4a2b6
Tushar Surve <ttusharsurve567@gmail.com>
2021-04-17 13:40:22 +0530

    Splash Screen

---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Splash Screen'),
+      ),
+    );
+  }
+}
