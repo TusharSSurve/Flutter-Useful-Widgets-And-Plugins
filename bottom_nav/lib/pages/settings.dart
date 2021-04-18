2e7190d76095d6a58a873f8c82c5fd675f247651
Tushar Surve <ttusharsurve567@gmail.com>
2021-04-18 13:23:46 +0530

    bottom nav

---
+++ bottom_nav/lib/pages/settings.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Setting extends StatefulWidget {
+  @override
+  _SettingState createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Setting'),
+      ),
+    );
+  }
+}
