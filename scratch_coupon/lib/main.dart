814e64997fef5049c8cf9546bf480370a0764621
Tushar Surve <ttusharsurve567@gmail.com>
2021-04-28 19:53:30 +0530

    Updated main

--- scratch_coupon/lib/main.dart
+++ scratch_coupon/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:scratcher/scratcher.dart';
 
@@ -19,26 +20,116 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ScratchCoupon extends StatelessWidget {
+class ScratchCoupon extends StatefulWidget {
+  @override
+  _ScratchCouponState createState() => _ScratchCouponState();
+}
+
+class _ScratchCouponState extends State<ScratchCoupon> {
+  ConfettiController controller;
+
+  @override
+  void initState() {
+    controller = ConfettiController(duration: Duration(seconds: 2));
+    super.initState();
+  }
+
+  Future<void> scratchCardDialog(BuildContext context, scratchKey) {
+    return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            title: Align(
+                alignment: Alignment.center,
+                child: Text("You've won a scratch card",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ))),
+            content: Scratcher(
+              accuracy: ScratchAccuracy.low,
+              image: Image.asset(
+                'assets/images/outerimage.png',
+              ),
+              key: scratchKey,
+              brushSize: 50,
+              threshold: 75,
+              onScratchEnd: () {
+                if (mounted) {
+                  setState(() {
+                    controller.play();
+                  });
+                }
+                scratchKey.currentState.reveal(duration: Duration(seconds: 2));
+              },
+              child: Container(
+                height: 230,
+                width: 300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/images/newimage.png',
+                      fit: BoxFit.contain,
+                      width: 150,
+                      height: 150,
+                    ),
+                    Column(
+                      children: [
+                        ConfettiWidget(
+                          confettiController: controller,
+                          colors: [
+                            Colors.red,
+                            Colors.blue,
+                            Colors.orange,
+                            Colors.purple,
+                            Colors.lightBlue,
+                          ],
+                          blastDirectionality: BlastDirectionality.explosive,
+                          emissionFrequency: 0,
+                          numberOfParticles: 20,
+                        ),
+                        Text("You've won")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     final scratchKey = GlobalKey<ScratcherState>();
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Scratcher(
-        key: scratchKey,
-        brushSize: 30,
-        threshold: 75,
-        color: Colors.grey.shade800,
-        onChange: (value) => print("Scratch progress: $value%"),
-        onScratchEnd: () {
-          Future.delayed(Duration(seconds: 2));
-          scratchKey.currentState.reveal(duration: Duration(seconds: 2));
-        },
-        onThreshold: () => print('object'),
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Rewards',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: GestureDetector(
+        onTap: () => scratchCardDialog(context, scratchKey),
         child: Container(
-          height: 300,
-          width: 300,
-          color: Colors.blue,
+          height: width / 2,
+          width: width / 2,
+          margin: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  'assets/images/outerimage.png',
+                )),
+            borderRadius: BorderRadius.circular(15),
+          ),
         ),
       ),
     );
